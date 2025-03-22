ecd7267e143493d78eb6436551b0dabf3215448e
Emmanoel Coutinho <coutinho.codes@gmail.com>
2025-03-22 12:36:38 -0300

    feat: Add Adopt website integration by including meta tag and script in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,16 @@ export default function Document() {
       <Head>
         <link rel="shortcut icon" type="imagex/png" href="/logo-si.ico" />
         <title>Santos Imóveis</title>
+
+        <meta
+          name="adopt-website-id"
+          content="67911adb-8b3b-4d73-ad52-afe0eb84956e"
+        />
+        <script
+          async
+          src="//tag.goadopt.io/injector.js?website_code=67911adb-8b3b-4d73-ad52-afe0eb84956e"
+          className="adopt-injector"
+        ></script>
       </Head>
       <body>
         <Main />
